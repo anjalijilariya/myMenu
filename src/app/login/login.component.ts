28a35de8a152f746a99c036e2e3952c74544a31e
anjalijilariya <202101503@daiict.ac.in>
2025-01-16 12:14:42 +0530

    added multiple user logins, password hidden, submit button works on pressing enter

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,30 +9,51 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  username = ['admin1', 'admin2' , 'client1', 'client2'];
+  usertype = ['admin', 'admin', 'customer', 'customer'];
+  password = ['123', '456', '789', 'abc'];
+  check: boolean = true;
 
   ngOnInit(): void {
   }
-  name: string;
-  passwd: string;
+  loginForm: FormGroup;
+
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      name: ['', Validators.required], // Form control for 'Name' with validation
+      passwd: ['', Validators.required], // Form control for 'Password' with validation
+    });
+  }
 
   loginCheck()
   {
-    if(this.name==="admin" && this.passwd === "123")
-    {
-      alert("Admin login successful");
-      this.router.navigate(['admin']);
+    // if(this.name==="admin" && this.passwd === "123")
+    // {
+    //   alert("Admin login successful");
+    //   this.router.navigate(['admin']);
+    // }
+
+    // else if(this.name==="customer" && this.passwd === "456")
+    // {
+    //   alert("Customer login successful");
+    //   this.router.navigate(['customer']);
+    // }
+
+    // else
+    // {
+    //   alert("Invalid Credentials");
+    // }
+    console.log(this.loginForm.value);
+    for (let index = 0; index < this.username.length; index++) {
+      if(this.loginForm.value.name===this.username[index] && this.loginForm.value.passwd === this.password[index])
+      {
+        alert( this.usertype[index] + " login successful");
+        this.router.navigate([this.usertype[index]]);
+        this.check = false;
+      }
     }
 
-    else if(this.name==="customer" && this.passwd === "456")
-    {
-      alert("Customer login successful");
-      this.router.navigate(['customer']);
-    }
-
-    else
-    {
+    if(this.check)
       alert("Invalid Credentials");
-    }
   }
 }
